08489ccb69b21925f9b74f1d91f588b8a67638ea
den <d.skunov@gmail.com>
2023-01-24 16:36:54 +0300

    portrait layout for testing

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import './ScanPage.css';
+import './CodeInput.css';
+
 
 const ScanPage = () => {
+    const CodeInput = () => {
+        return (
+                <div className="code-input">
+                    <div className="code">1</div>
+                    <div className="code">1</div>
+                    -
+                    <div className="code">1</div>
+                    <div className="code">1</div>
+                    <div className="code">1</div>
+                    -
+                    <div className="code">1</div>
+                    <div className="code">1</div>
+                </div>
+            )
+        }
+
     return (
         <div className="wrapper">
             <div className="scan-preview">
                 empty
                 <hr></hr>
-                scan preview
                 <div className="scan">
                     scan
                 </div>
             </div>
             <div className="result-preview">
                 result preview
+                <CodeInput />
             </div>
         </div>
     )
